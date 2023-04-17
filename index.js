ae695ce93818267fcc430e1f298a931a6e25d1a3
William Wong <william.wai.lim.wong@gmail.com>
2023-04-17 12:38:10 -0400

    send data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        const element = document.createElement("div");
+        document.body.appendChild(element);
+        document.querySelector('div').innerHTML = object.id;
+    })
+    .catch(function (error) {
+        document.querySelector('div').innerHTML = error.message;
+    })
+}
